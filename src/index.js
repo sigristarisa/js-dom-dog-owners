6a65e6342335f5cb439a537078e3dc9e304a7104
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-20 17:53:26 +0200

    created the form

--- src/index.js
+++ src/index.js
@@ -23,11 +23,17 @@ const dogBtn = document.createElement("button");
 dogBtn.innerText = "Good dog";
 dogListBtnDiv.style.display = "none";
 
+// form
+const addButton = document.getElementById("dogs-list__button--add");
+const formContainer = document.getElementById("main__dog-section-form");
+formContainer.style.display = "none";
+
 // append all elements
 dogListDescDiv.append(bioTitle, bioText);
 dogListBtnDiv.append(isNaughty, dogBtn);
 dogSection.append(dogName, img, dogListDescDiv, dogListBtnDiv);
 
+// ********* PART 1 ********* //
 data.forEach((dog) => {
   // Header
   const dogsList = document.getElementById("dogs-list");
@@ -36,7 +42,9 @@ data.forEach((dog) => {
   dogListButton.innerText = dog.name;
   dogsList.append(dogListButton);
 
+  // when dog name is clicked
   dogListButton.addEventListener("click", () => {
+    formContainer.style.display = "none";
     img.style.display = "block";
     dogListDescDiv.style.display = "block";
     dogListBtnDiv.style.display = "grid";
@@ -45,3 +53,32 @@ data.forEach((dog) => {
     bioText.innerText = dog.bio;
   });
 });
+
+// ********* PART 2 ********* //
+
+addButton.addEventListener("click", () => {
+  dogName.innerText = "add a new Dog";
+  img.style.display = "none";
+  dogListDescDiv.style.display = "none";
+  dogListBtnDiv.style.display = "none";
+
+  formContainer.style.display = "block";
+});
+
+/*
+ <section class="main__dog-section">
+      <h2>Add a new Dog</h2>
+      <form class="form">
+
+        <label for="name">Dog's name</label>
+        <input type="text" id="name" name="name">
+
+        <label for="image">Dog's picture</label>
+        <input type="url" id="image" name="image">
+
+        <label for="bio">Dog's bio</label>
+        <textarea rows="5" id="bio" name="bio"></textarea>
+
+        <input type="submit" id="submit" name="submit" value="Let's add a dog!" class="form__button">
+      </form>
+      */
