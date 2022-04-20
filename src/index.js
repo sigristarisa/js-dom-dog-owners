c2f706060cb84594fcbd3476497651deb2c61cb8
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-20 16:04:33 +0200

    created the header with buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-console.log(data);
+// console.log(data);
 
-// WRITE YOUR CODE BELOW!
+data.forEach((dog) => {
+  const dogsList = document.getElementById("dogs-list");
+  const dogListButton = document.createElement("li");
+  dogListButton.setAttribute("class", "dogs-list__button");
+  dogListButton.innerText = dog.name;
+  dogsList.append(dogListButton);
+});
